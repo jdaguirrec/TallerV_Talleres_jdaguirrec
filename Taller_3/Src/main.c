f6065c233636663c141d8afc0e3dddd60ad1c90f
jdaguirrec <jdaguirrec@unal.edu.co>
2022-09-02 16:50:56 -0500

    Nuevo proyecto

---
+++ Taller_3/Src/main.c
@@ -0,0 +1,17 @@
+/**
+ ******************************************************************************
+ * @file           : main.c
+ * @author         : jdaguirrec
+ * @brief          : Taller número 3 con el monitor
+ ******************************************************************************
+ */
+// Librería variables
+#include <stdint.h>
+
+// Librería para tipos de variables booleanas
+#include <stdbool.h>
+
+// Librería para operaciones matemáticas
+#include <math.h>
+
+#define NOP() __asm("NOP")
