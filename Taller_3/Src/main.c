260e09cfbb898d773f5a5188f891d49360050215
jdaguirrec <jdaguirrec@unal.edu.co>
2022-09-02 18:06:40 -0500

    Adición de líneas de código

--- Taller_3/Src/main.c
+++ Taller_3/Src/main.c
@@ -15,3 +15,34 @@
 #include <math.h>
 
 #define NOP() __asm("NOP")
+
+void miPrimeraFuncion(void);
+
+uint8_t getMaxChar(void);
+
+int main(void){
+
+	miPrimeraFuncion();
+
+	while(1){
+
+		miPrimeraFuncion();
+
+	}
+
+}
+
+void miPrimeraFuncion(void){
+
+	// Manejo de variables booleanas
+
+	uint8_t miPrimeraBandera = true;
+	uint8_t miSegundaBandera = false;
+
+	uint8_t XOR = miPrimeraBandera ^ miSegundaBandera;
+	uint8_t OR = miPrimeraBandera || miSegundaBandera;
+	uint8_t AND = miPrimeraBandera && miSegundaBandera;
+
+	XOR ^= 1;
+	XOR ^= 1;
+}
